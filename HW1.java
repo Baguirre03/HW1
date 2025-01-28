6297e31cd157da71a819a3e4e3286dc9c2b03454
Ben Aguirre <baguirre@luc.edu>
2025-01-28 13:13:12 -0600

    add: linkedList, stack, algoAnalysis implementation

--- HW1.java
+++ HW1.java
@@ -22,14 +22,15 @@ public class HW1 {
      * contains a single integer values.
      *
      * Methods:
-     *  - void   sortInserted(val)     - Inserts value 'val' into the linked-list in
-     *                                   sorted fashion
-     *  - void   removeElementsLT(val) - Removed values in the linked-list that are less
-     *                                   than 'val'
-     *  - void   removeElement(val)    - Removes all values in the linked list of
-     *                                   value 'val'
-     *  - String toString()            - coverts and returns the linked-lists as a string
-     *                                   delimited by brackets []
+     * - void sortInserted(val) - Inserts value 'val' into the linked-list in
+     * sorted fashion
+     * - void removeElementsLT(val) - Removed values in the linked-list that are
+     * less
+     * than 'val'
+     * - void removeElement(val) - Removes all values in the linked list of
+     * value 'val'
+     * - String toString() - coverts and returns the linked-lists as a string
+     * delimited by brackets []
      *
      */
 
@@ -38,13 +39,13 @@ public class HW1 {
             int data;
             Node next;
 
-            Node(int d)  {        // Constructor
+            Node(int d) { // Constructor
                 data = d;
                 next = null;
             }
         }
-        Node head;                // head of Linked-list
 
+        Node head; // head of Linked-list
 
         /*
          * Method sortedInsert() - this method will insert a new node to the
@@ -53,13 +54,13 @@ public class HW1 {
          * the linked-list.
          *
          */
-        public void sortedInsert ( int data ) {
+        public void sortedInsert(int data) {
             Node new_node = new Node(data);
 
             new_node.next = null;
 
             // Special case for head node.
-            if (this.head == null || head.data >= new_node.data ) {
+            if (this.head == null || head.data >= new_node.data) {
                 new_node.next = head;
                 head = new_node;
             } else {
@@ -77,7 +78,6 @@ public class HW1 {
             return;
         }
 
-
         /*
          * Method removeElementsLT() - this method removes all nodes that contain a
          * value that is less than the provided parameter 'ltValue'.
@@ -85,32 +85,74 @@ public class HW1 {
          * The method will invoke the method removeElements for each element
          * found in the linked-list that is less than thr parameter value passed.
          */
-        public void removeElementsLT ( int ltValue ) {
-
-            // YOUR CODE GOES HERE
-
+        public void removeElementsLT(int ltValue) {
+            // decalre head of node into a current variableut
+            Node cur = this.head;
+            Node prev = null;
+            // while there is a next node loop through
+            // since this is a sorted linkedlist we do not need to remove each node
+            // instead we find the first node that is greater than value
+            // then splice from there
+            boolean nodeFound = false;
+            while (cur.next != null && !nodeFound) {
+                if (cur.data >= ltValue) {
+                    // remove node
+                    // if first node is greater than, dont remove anything
+                    if (prev != null) {
+                        // set the head to cur (splicing node)
+                        this.head = cur;
+                    }
+                    // end loop with while loop condition
+                    nodeFound = true;
+                }
+                // move pointers
+                prev = cur;
+                cur = cur.next;
+            }
             return;
         }
-
 
         /*
          * Method removeElement() - this method removes all nodes that contain a
          * value equal to the value the provided parameter 'value'.
          */
 
-        public void removeElement ( int value ) {
-
-            // YOUR CODE GOES HERE
-
+        public void removeElement(int value) {
+            // decalre head of node into a current variableut
+            Node cur = this.head;
+            Node prev = null;
+            // while there is a next node loop through
+            // since this is a sorted linkedlist we do not need to remove each node
+            // keep track of current and previous pointers and a lessThan boolean
+            // once we get a number higher than value we can end loop
+            // since it is a sorted list
+            boolean lessThan = false;
+            while (cur.next != null && !lessThan) {
+                if (cur.data > value)
+                    lessThan = true;
+                if (cur.data == value) {
+                    // remove node
+                    // if first node is greater than, dont remove anything
+                    if (prev == null) {
+                        // remove head
+                        this.head = cur.next;
+                    } else {
+                        // else set prev.next to cur.next to remove current node from list
+                        prev.next = cur.next;
+                    }
+                }
+                // move pointers
+                prev = cur;
+                cur = cur.next;
+            }
             return;
         }
-
 
         /*
          * Method toString() - this is a helper method for printing / constructing
          * a string object from the linked-list.
          */
-        public String toString () // Method to output the LinkedList as a String
+        public String toString() // Method to output the LinkedList as a String
         {
             String output = "[";
             Node currNode = this.head;
@@ -123,9 +165,6 @@ public class HW1 {
 
     } // End class LinkedList
 
-
-
-
     /*
      * Class Stacks
      *
@@ -136,11 +175,11 @@ public class HW1 {
      * your solution.
      *
      * Methods:
-     *  - boolean isPalindrome(string)   - method returns true or false if 'string'
-     *                                     is a palindrome
-     *  - int     findlargestK(stack, k) - method accepts a stack and returns the
-     *                                     the largest index in the stack containing
-     *                                     value 'k' (stack index starts at 0)
+     * - boolean isPalindrome(string) - method returns true or false if 'string'
+     * is a palindrome
+     * - int findlargestK(stack, k) - method accepts a stack and returns the
+     * the largest index in the stack containing
+     * value 'k' (stack index starts at 0)
      *
      */
 
@@ -151,19 +190,48 @@ public class HW1 {
          * or 'false' on if the passed in parameter string is a palindrome or not.
          *
          * The routine should be case insensitive! Meaning 'RaCe cAr' is a palindrome.
-         * Moreover, spaces are ignore, so both 'race car' and 'racecar' are plaindromes.
+         * Moreover, spaces are ignore, so both 'race car' and 'racecar' are
+         * plaindromes.
          *
          * The method should utilize the provided Stack class.
          */
         public static boolean isPalindrome(String input) {
-
             Stack<Character> stack = new Stack<>();
             input = input.toLowerCase().replaceAll("\\s+", "");
+            // find half of the input
+            int half = input.length() / 2;
+            // declare an index to keep track of current char
+            int indx = 0;
+            // while index is less than half, of the input
+            while (indx < half) {
+                // push the character into the stack
+                stack.push(input.charAt(indx));
+                indx += 1;
+            }
+            // if the length is odd, then the middle character does not matter,
+            // increase index by one
+            if (!(input.length() % 2 == 0)) {
+                indx += 1;
+            }
 
-            // Your CODE GOES HERE
-            return false;
+            // compare the second half of the input
+            while (indx < input.length()) {
+                // if the top of the stack element equals the current index, then it matches
+                // palindrome wise
+                if (stack.lastElement().equals(input.charAt(indx))) {
+                    // when that happens pop from stack
+                    stack.pop();
+                }
+
+                // increase indx
+                indx += 1;
+            }
+            // finally if there is still chars in the stack then it is NOT palindrome,
+            // if it is empty then all characters matched on both sides and palindrome is
+            // true
+            return stack.size() == 0;
+
         }
-
 
         /*
          * Method findLargestk() - This method will return the largest index
@@ -181,14 +249,41 @@ public class HW1 {
          * completed, place them all back in teh original stack.
          */
         public static int findLargestK(Stack<Integer> stack, int k) {
+            // implement temp stack
+            Stack<Integer> tmpStack = new Stack<>();
+            // if not stack return 0
+            if (stack.size() == 0)
+                return 0;
+            // simplified: return the highest index for the integer K sitting in the stack
+            // keep track of current index to return
+            int curIndex = stack.size();
+            // if we find the number we want to stop while loop
+            boolean found = false;
+            // while we have the stack and num not found
+            while (stack.size() > 0 && !found) {
+                // grab the top number
+                int topNumber = stack.peek();
+                // push the top number into the temporary stack
+                tmpStack.push(stack.pop());
+                // if top number == k then stop while loop
+                if (topNumber == k) {
+                    found = true;
+                }
 
-            // YOUR CODE GOES HERE
-            return -1;
+                // decrease index to match correct el
+                curIndex -= 1;
+            }
+
+            // push the tmpStack back into the original stack to keep original state
+            while (tmpStack.size() > 0) {
+                stack.push(tmpStack.pop());
+            }
+
+            // return correct index of highest K element in stack
+            return curIndex;
         }
 
-    }  // End class Stacks
-
-
+    } // End class Stacks
 
     /*******************************
      *
@@ -202,46 +297,47 @@ public class HW1 {
     public static int algorithmAnalysis1(int n, int m) {
         int a = 0, b = 0;
 
-        for (int i=0; i < n; i++)
-            a+= Math.random();
-
-        for (int j=0; j < m; j++)
-            b+= Math.random();
+        // runs N time
+        for (int i = 0; i < n; i++)
+            a += Math.random();
+        // runs M time
+        for (int j = 0; j < m; j++)
+            b += Math.random();
 
         /*
          * Select the correct option listed below:
-         *   1. O(N * M) time, O(1) space
-         *   2. O(N + M) time, O(N + M) space
-         *   3. O(N + M) time, O(1) space
-         *   4. O(N * M) time, O(N + M) space
+         * 1. O(N * M) time, O(1) space
+         * 2. O(N + M) time, O(N + M) space
+         * 3. O(N + M) time, O(1) space
+         * 4. O(N * M) time, O(N + M) space
          *
          * TODO: return the answer (which option is correct), in the return statement
-        */
+         */
 
         // RETURN THE CORRECT OPTION NUMBER LISTED ABOVE
-        return -1;
+        return 3;
     }
-
 
     public static int algorithmAnalysis2(int n) {
         int i, j, k = 0;
-        for (i = n/2; i <= n; i++)
-            for ( j = 2; j <= n; j = j*2 )
-                k+= n/2;
+        for (i = n / 2; i <= n; i++)
+            // n == 12; i == 6 (n / 2) time
+            for (j = 2; j <= n; j = j * 2)
+                // j == 2; j = j * 2 (log n time)
+                k += n / 2;
 
         /*
          * Select the correct option listed below:
-         *   1. O(N) time
-         *   2. O(N log N) time
-         *   3. O(N^2) time
-         *   4. O(N^2Log n) time
+         * 1. O(N) time
+         * 2. O(N log N) time
+         * 3. O(N^2) time
+         * 4. O(N^2Log n) time
          *
          * TODO: return the answer (which option is correct), in the return statement
          */
 
         // RETURN THE CORRECT OPTION LISTED ABOVE
-        return -1;
+        return 2;
     }
 
 }
-
